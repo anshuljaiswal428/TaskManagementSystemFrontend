d83e59415fdecca510cad5b56a248fba921dce11
ANSHUL JAISWAL <anshul.jaiswal428@gmail.com>
2025-05-04 16:46:48 +0530

    url changed

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL = "https://taskmanagementsystembackend.onrender.com";
 
 // utils/apiPaths.js
 export const API_PATHS = {
